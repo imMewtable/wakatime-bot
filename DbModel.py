48c8be587dae7644328ce079994d1a42ed4051bf
Joshua Carney <joshuakcarney@gmail.com>
2022-01-13 21:08:18 -0500

    server config model code

--- DbModel.py
+++ DbModel.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 from dotenv import load_dotenv
 from peewee import *
@@ -19,25 +20,41 @@ db = MySQLDatabase(
 
 
 class BaseModel(Model):
-    """A base model that will use our Sqlite database."""
+    """A base model that will use our MySQL database."""
 
     class Meta:
         database = db
-        primary_key = CompositeKey('discord_username', 'server_id')
 
 
+def default_time():
+    return datetime.time(0, 0, 0)
+
+
+# Class that describes how Wakabot will act on a per-server basis
+class ServerConfig(BaseModel):
+    server_id = BigIntegerField(primary_key=True)                                   # Discord's server id
+    display_weekly = BooleanField(null=False, default=False)                        # Automatically display user scoreboard weekly
+    display_monthly = BooleanField(null=False, default=False)                       # Automatically display user scoreboard monthly
+    user_scoreboard_count = IntegerField(null=False, default=5)                     # Max number of users to display on any given scoreboard
+    auto_scoreboard_display = TimeField(null=False, default=default_time) # When to display the scoreboard (UTC) (defaults to 00:00:00 UTC or 7PM EST)
+
+
+# Class that describes a user that wakabot keeps track of
 class WakaData(BaseModel):
-    discord_username = CharField(null=False, max_length=40)
-    wakatime_username = CharField(null=True, max_length=40)
-    auth_token = CharField(null=True, max_length=100)
-    refresh_token = CharField(null=True, max_length=100)
-    server_id = BigIntegerField(null=False)
+    discord_username = CharField(null=False, max_length=40) # Discord member's username
+    wakatime_username = CharField(null=True, max_length=40) # Wakatime username (not used for anything rn)
+    auth_token = CharField(null=True, max_length=100)       # Access token used to access wakatime API as authenticated user
+    refresh_token = CharField(null=True, max_length=100)    # Refresh token used to get new access tokens
+    server_id = BigIntegerField(null=False)                 # Discord server ID
+
+    class Meta:
+        primary_key = CompositeKey('discord_username', 'server_id')
 
 
 # Self explanatory
 def init_tables():
     db.connect()
-    db.create_tables([WakaData])
+    db.create_tables([WakaData, ServerConfig])
     db.close()
 
 
@@ -47,6 +64,55 @@ def initialize_user_data(discord_user, serverid):
     try:
         db.connect()
         code = WakaData.create(discord_username=discord_user, server_id=serverid)
+        db.close()
+        return code
+    except Exception as e:
+        print(e)
+        db.close()
+        return None
+
+
+# Used when an admin first tries to use !config command.
+# Initializes an entry with the server ID with appropriate defaults
+def initialize_server_data(server_id):
+    try:
+        db.connect()
+        code = ServerConfig.create(server_id=server_id)
+        db.close()
+        return code
+    except Exception as e:
+        print(e)
+        db.close()
+        return None
+
+
+def update_server_config(server_id, display_weekly=None, display_monthly=None,
+                         user_scoreboard_count=None, auto_display_scoreboard=None):
+    """
+    Updates the server's config with the information passed in.
+    Parameters are optional other than server_id, which is required.
+
+    :param server_id: discord server's unique id
+    :param display_weekly: should be None or True/False
+    :param display_monthly: should be None or True/False
+    :param user_scoreboard_count: should be an integer
+    :param auto_display_scoreboard: must be a time - datetime.time(0, 0, 0)
+    :return: an int that    describes how many rows in the DB were changed
+    """
+    try:
+        db.connect()
+        data = ServerConfig.get(ServerConfig.server_id == server_id)
+
+        if display_weekly is not None:
+            data.display_weekly = display_weekly
+        if display_monthly is not None:
+            data.display_monthly = display_monthly
+        if user_scoreboard_count is not None:
+            data.user_scoreboard_count = user_scoreboard_count
+        if auto_display_scoreboard is not None:
+            data.auto_scoreboard_display = auto_display_scoreboard
+
+        code = data.save()
         db.close()
         return code
     except Exception as e:
